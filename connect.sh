7e1dd28ce50cea44f6694c4cff21cd6b723c4c9d
Your Name <you@example.com>
2025-05-02 03:15:16 +0200

    feat: update patch for production

--- connect.sh
+++ connect.sh
@@ -7,7 +7,7 @@
 # Produzione
 
 # gcloud container clusters get-credentials autoblog-prod-gke-cluster --zone europe-west4-a --project gcp-automated-blog-prod
-# gcloud compute ssh --zone "europe-west4-a" "autoblog-prod-bastion-vm" --tunnel-through-iap --project "gcp-automated-blog-prod" -- -fNL 8443:10.0.1.11:443
+# gcloud compute ssh --zone "europe-west4-a" "autoblog-prod-bastion-vm" --tunnel-through-iap --project "gcp-automated-blog-prod" -- -fNL 8445:10.0.1.11:443
 
-# kubectl config set-cluster gke_gcp-automated-blog-prod_europe-west4-a_autoblog-prod-gke-cluster --server=https://127.0.0.1:8443
+# kubectl config set-cluster gke_gcp-automated-blog-prod_europe-west4-a_autoblog-prod-gke-cluster --server=https://127.0.0.1:8445
 # kubectl config set-cluster gke_gcp-automated-blog-prod_europe-west4-a_autoblog-prod-gke-cluster --tls-server-name=10.0.1.11
