8a5c91091d76d6468ade1a9afafb71fe45615fe4
Your Name <you@example.com>
2025-05-09 11:16:32 +0200

    feat: deploy on prod

--- connect.sh
+++ connect.sh
@@ -1,4 +1,9 @@
 # gcloud container clusters get-credentials autoblog-test-gke-cluster --zone europe-west4-a --project gcp-automated-blog-test
+#   export PRIVATE_IP_GKE_CLUSTER=$(gcloud container clusters describe autoblog-test-gke-cluster \
+#     --project "gcp-automated-blog-test" \
+#     --zone europe-west4-a \
+#     --format="get(privateClusterConfig.privateEndpoint)")
+
 # kubectl config set-cluster gke_gcp-automated-blog-test_europe-west4-a_autoblog-test-gke-cluster --server=https://127.0.0.1:8443
 # kubectl config set-cluster gke_gcp-automated-blog-test_europe-west4-a_autoblog-test-gke-cluster --tls-server-name=10.0.0.11
 
@@ -7,7 +12,17 @@
 # Produzione
 
 # gcloud container clusters get-credentials autoblog-prod-gke-cluster --zone europe-west4-a --project gcp-automated-blog-prod
-# gcloud compute ssh --zone "europe-west4-a" "autoblog-prod-bastion-vm" --tunnel-through-iap --project "gcp-automated-blog-prod" -- -fNL 8445:10.0.1.11:443
+# export PRIVATE_IP_GKE_CLUSTER=$(gcloud container clusters describe autoblog-prod-gke-cluster \
+# --project "gcp-automated-blog-prod" \
+# --zone europe-west4-a \
+# --format="get(privateClusterConfig.privateEndpoint)")
+
+# gcloud compute ssh --zone "europe-west4-a" "autoblog-prod-bastion-vm" --tunnel-through-iap --project "gcp-automated-blog-prod" -- -fNL 8445:$PRIVATE_IP_GKE_CLUSTER:443
 
 # kubectl config set-cluster gke_gcp-automated-blog-prod_europe-west4-a_autoblog-prod-gke-cluster --server=https://127.0.0.1:8445
 # kubectl config set-cluster gke_gcp-automated-blog-prod_europe-west4-a_autoblog-prod-gke-cluster --tls-server-name=10.0.1.11
+
+#   export PRIVATE_IP_GKE_CLUSTER=$(gcloud container clusters describe autoblog-test-gke-cluster \
+#     --project "gcp-automated-blog-test" \
+#     --zone europe-west4-a \
+#     --format="get(privateClusterConfig.privateEndpoint)")
